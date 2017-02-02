35acd07b89a91734585e7e66f346caecad41eb1f
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-02 12:14:51 +0000

    PSM on by default.

--- jasmine/spec/ThermostatSpec.js
+++ jasmine/spec/ThermostatSpec.js
@@ -4,22 +4,13 @@ describe("Thermostat", function() {
     thermostat = new Thermostat();
   });
 
+describe("Default Thermostat", function(){
   it('has a default temperature of 20 degrees', function(){
     expect(thermostat.temperature()).toEqual(20);
   });
 
   it('has a minimum temperature of 10 degrees', function(){
     expect(thermostat._minTemperature).toEqual(10);
-  });
-
-  it('up function increases temperature', function(){
-    thermostat.increaseTemperature()
-    expect(thermostat.temperature()).toEqual(21);
-  });
-
-  it('down function decreases temperature', function(){
-    thermostat.decreaseTemperature()
-    expect(thermostat.temperature()).toEqual(19);
   });
 
   it('the minimum temperature is 10 degrees', function() {
@@ -29,5 +20,47 @@ describe("Thermostat", function() {
     };
     expect(function(){thermostat.decreaseTemperature()}).toThrow('Temperature cannot be decreased below 10 degrees');
   });
+});
+
+describe("Changing temperature", function(){
+  it('up function increases temperature', function(){
+    thermostat.increaseTemperature()
+    expect(thermostat.temperature()).toEqual(21);
+  });
+
+  it('down function decreases temperature', function(){
+    thermostat.decreaseTemperature()
+    expect(thermostat.temperature()).toEqual(19);
+  });
+});
+
+describe("Power Saving Mode", function(){
+  it('switches PSM\'s state', function() {
+    thermostat.switchPSM();
+    expect(thermostat.isOnPSM()).toBe(false);
+  });
+
+  it('checks if power saving mode is on by default', function(){
+    expect(thermostat.isOnPSM()).toBe(true);
+  });
+
+  it('If power saving mode is on, the maximum temperature is 25 degrees', function(){
+    var times = 5;
+    for(var i=0; i < times; i++){
+      thermostat.increaseTemperature();
+    };
+    expect(function(){thermostat.increaseTemperature()}).toThrow('Maximum temperature on PSM is 25 degrees');
+  });
+
+  it('If power saving mode is off, the maximum temperature is 32 degrees', function(){
+    thermostat.switchPSM();
+    var times = 12;
+    for(var i=0; i < times; i++){
+      thermostat.increaseTemperature();
+    };
+    expect(function(){thermostat.increaseTemperature()}).toThrow('Maximum temperature is 32 degrees');
+  });
+
+});
 
 });
--- jasmine/src/Thermostat.js
+++ jasmine/src/Thermostat.js
@@ -1,9 +1,12 @@
 const DEFAULT_TEMPERATURE = 20;
 const MIN_TEMPERATURE = 10;
+const MAX_POWER_SAVING_MODE = 25;
+const MAX_TEMPERATURE = 32;
 
 function Thermostat(){
   this._defaultTemperature = DEFAULT_TEMPERATURE;
   this._minTemperature = MIN_TEMPERATURE;
+  this._onPSM = true;
 };
 
 Thermostat.prototype.temperature = function(){
@@ -11,6 +14,11 @@ Thermostat.prototype.temperature = function(){
 };
 
 Thermostat.prototype.increaseTemperature = function(){
+  if (this.isOnPSM() && this.temperature() >= MAX_POWER_SAVING_MODE) {
+    throw('Maximum temperature on PSM is 25 degrees');
+  } else if (!this.isOnPSM() && this.temperature() >= MAX_TEMPERATURE) {
+    throw('Maximum temperature is 32 degrees');
+  };
   this._defaultTemperature ++;
 };
 
@@ -19,4 +27,12 @@ Thermostat.prototype.decreaseTemperature = function(){
     throw('Temperature cannot be decreased below 10 degrees');
   };
   this._defaultTemperature --;
+};
+
+Thermostat.prototype.isOnPSM = function() {
+  return this._onPSM
+};
+
+Thermostat.prototype.switchPSM = function() {
+  this._onPSM = !this._onPSM;
 };
